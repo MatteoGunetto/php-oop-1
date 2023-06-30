6bc579b2156e8e6866768c902c8f142bf07b90b3
MatteoGunetto <Matteogunetto@tiscali.it>
2023-06-30 16:40:24 +0200

    completo/bonus 1

--- index.php
+++ index.php
@@ -1,36 +1,130 @@
 <?php
-class Movie{
+
+// creazione classe movie
+class Movie
+{
     public $title;
     public $director;
     public $duration;
     private $year_of_release;
+    private $genreArray = array();
 
-    public function setYear($year_of_release){
-        if(is_string($year_of_release)){
+    function __construct(string $_title, int $_duration, string $_director = 'Russo brothers')
+    {
+        $this->title = $_title;
+        $this->director = $_director;
+        $this->duration = $_duration;
+    }
+
+    public function setYear($year_of_release)
+    {
+        if (is_string($year_of_release)) {
             echo 'Errore, inserisci un anno valido';
-        }
-        else{
+        } else {
             $this->year_of_release = $year_of_release;
         }
     }
-    public function getYear(){
+    public function getYear()
+    {
         return $this->year_of_release;
+    }
+
+    // funzioni per aggiungere più di un genere
+
+    public function addGenre($genre)
+    {
+        $this->genreArray[] = $genre;
+    }
+
+    public function getGenres()
+    {
+        return $this->genreArray;
     }
 }
 
-$star_wars_episode_IV = new Movie();
-$star_wars_episode_IV->title = 'Star Wars: Episode IV';
-$star_wars_episode_IV->director = 'George Lucas';
-$star_wars_episode_IV->duration = 121;
-$star_wars_episode_IV->setYear(1977);
-$star_wars_episode_IV->getYear();
-var_dump($star_wars_episode_IV);
+// istanza di due oggetti con classe Movie
+$movie1 = new Movie('Avengers: Infinity War', 149);
+$movie1->setYear(2018);
+$movie1->getYear();
+$movie1->addGenre('Azione');
+$movie1->addGenre('Fantascienza');
+$movie1->addGenre('Avventura');
+$movie1->getGenres();
+// var_dump($movie1);
 
+echo '<br>';
+echo '<br>';
 
-$avengers_endgame = new Movie();
-$avengers_endgame->title = 'Avengers: Endgame';
-$avengers_endgame->director = 'Russo brothers';
-$avengers_endgame->duration = 181;
-$avengers_endgame->setYear(2019);
-$avengers_endgame->getYear();
-var_dump($avengers_endgame);
+$movie2 = new Movie('Avengers: Endgame', 181);
+$movie2->setYear(2019);
+$movie2->getYear();
+$movie2->addGenre('Azione');
+$movie2->addGenre('Fantascienza');
+$movie2->addGenre('Avventura');
+$movie2->getGenres();
+// var_dump($movie2);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP OOP 1</title>
+</head>
+
+<body>
+
+    <h1>Infinity Saga</h1>
+
+    <div>
+        <h2>
+            Titolo:
+            <?php echo $movie1->title ?>
+        </h2>
+        <h3>
+            Regista:
+            <?php echo $movie1->director ?>
+        </h3>
+        <h3>
+            Durata: <?php echo $movie1->duration ?> min
+        </h3>
+        <h3>
+            Anno di uscita: <?php echo $movie1->getYear() ?>
+        </h3>
+        <h3>
+            Genere: <?php
+            for ($i=0; $i < count($movie1->getGenres()); $i++) { 
+                echo $movie1->getGenres()[$i] . ' ';
+            } ?>
+        </h3>
+    </div>
+    <br>
+    <div>
+        <h2>
+            Titolo:
+            <?php echo $movie2->title ?>
+        </h2>
+        <h3>
+            Regista:
+            <?php echo $movie2->director ?>
+        </h3>
+        <h3>
+            Durata: <?php echo $movie2->duration ?> min
+        </h3>
+        <h3>
+            Anno di uscita: <?php echo $movie2->getYear() ?>
+        </h3>
+        <h3>
+            Genere: <?php
+            for ($i=0; $i < count($movie2->getGenres()); $i++) { 
+                echo $movie2->getGenres()[$i] . ' ';
+            } ?>
+        </h3>
+    </div>
+
+</body>
+
+</html>
